2d07fab6ee6973b1d088dd497365e5a03a008482
Gabrielle <gabrielle.a21@mhs.istts.ac.id>
2025-02-09 11:39:02 +0700

    revisi hilangin button di layout admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,10 +12,10 @@
 
 <body>
     <div class="container py-4">
-        <div class="d-flex justify-content-center gap-3">
+        <!-- <div class="d-flex justify-content-center gap-3">
             <a href="{{ url('/admin/products') }}" class="btn btn-outline-primary">Products</a>
             <a href="{{ url('/admin/employees') }}" class="btn btn-outline-secondary">Employees</a>
-        </div>
+        </div> -->
         @yield('content')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
